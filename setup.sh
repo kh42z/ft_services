c97cf62778796a01366c5b519745fccc5ade28b0
kh4z <thibaudy95@gmail.com>
2020-08-06 11:45:43 +0200

    standalone script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 USER=$(whoami)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/srcs"
+CONTAINERS=("mysql" "wordpress" "phpmyadmin" "nginx" "ftps" "influxdb" "grafana")
 
 if [ $(nproc) -lt 2 ]
 	then
@@ -12,7 +14,7 @@ if [ $MEM -lt 3062431 ]
 	echo "FT_SERVICES requires 3G of memory at least"
 	exit
 fi
-if ! id -nG "$USER" | grep -qw "docker" 
+if ! id -nG "$USER" | grep -qw "docker"
 	then
     echo "FT_SERVICES requires $USER to have access to docker"
 	echo run "sudo usermod -aG docker ${USER}"
@@ -25,7 +27,71 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 sudo mv minikube /usr/local/bin/
 minikube start --vm-driver=docker
 minikube addons enable metallb
-/bin/bash ./srcs/build.sh
-/bin/bash ./srcs/secrets.sh
-/bin/bash ./srcs/deploy.sh
+
+## SECRETS.SH
+echo "Enter username"
+read USERNAME
+echo "Enter password"
+read PASSWORD
+USERNAME=$(echo $USERNAME| tr -cd '[:alnum:].')
+PASSWORD=$(echo $PASSWORD| tr -cd '[:alnum:].')
+if [ ${#USERNAME} -lt 4 ]; then
+	echo "Username too short"
+	USERNAME="default"
+fi
+if [ ${#PASSWORD} -lt 4 ]; then
+	echo "Password too short"
+	PASSWORD="default"
+fi
+echo "Defaut login is $USERNAME:$PASSWORD"
+kubectl create secret generic ft-user --from-literal=username="$USERNAME" --from-literal=password="$PASSWORD"
+
+
+## BUILD.SH
+PREFIX="ft_services/"
+eval $(minikube docker-env)
+
+for CONTAINER in ${CONTAINERS[@]}; do
+	NAME=${PREFIX}${CONTAINER}
+	echo "Building: ${NAME}"
+	docker build -t ${NAME} ${DIR}/${CONTAINER} > /dev/null
+done
+
+## DEPLOY.SH
+NETWORK=$(echo $MINIKUBE_IP|awk -F . '{print $1"."$2"."$3"."}')
+
+sed 's/{RANGE}/'${NETWORK}'100-'${NETWORK}'200/' ${DIR}/k8s/metallb.template > ${DIR}/k8s/metallb.yaml
+kubectl apply -f ${DIR}/k8s/metallb.yaml
+
+for CONTAINER in ${CONTAINERS[@]}; do
+	NAME=${CONTAINER}
+	if [ -f ${DIR}/${NAME}/vol.yaml ]; then
+		echo "Creating vol: ${NAME}"
+		kubectl create -f ${DIR}/${NAME}/vol.yaml
+		kubectl create -f ${DIR}/${CONTAINER}/claim.yaml
+	fi
+done
+
+sleep 5
+for CONTAINER in ${CONTAINERS[@]}; do
+	NAME=${CONTAINER}
+	echo "Creating service: ${NAME}"
+	kubectl create -f ${DIR}/${NAME}/svc.yaml
+done
+
+for CONTAINER in ${CONTAINERS[@]}; do
+  EIP=$(kubectl describe svc ${CONTAINER}|grep Ingress|awk -F" " '{print $3}'|tr -d "\n ")
+	if [ ${CONTAINER} == "mysql" ]
+	then
+		EIP=$(kubectl describe svc wordpress|grep Ingress|awk -F" " '{print $3}'|tr -d "\n ")
+	fi
+	NAME=${CONTAINER}
+	echo "Creating deployement: ${NAME}"
+	if [ -f ${DIR}/${NAME}/deploy.template ]; then
+		sed 's/{EIP}/'${EIP}'/' ${DIR}/${NAME}/deploy.template > ${DIR}/${NAME}/deploy.yaml
+	fi
+	kubectl create -f ${DIR}/${NAME}/deploy.yaml
+done
+
+### DASHBOARD
 minikube dashboard
